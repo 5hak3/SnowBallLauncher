c908e98c2752b03034a4bccf47644c5ea8a6dcc1
5hak3 <shake.salmo@gmail.com>
2022-01-22 12:25:58 +0900

    [ADD] hit sound and hit particle

--- src/main/java/net/azisaba/lobby/SnowBallLauncher.java
+++ src/main/java/net/azisaba/lobby/SnowBallLauncher.java
@@ -2,16 +2,21 @@ package net.azisaba.lobby;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Arrays;
 
@@ -40,11 +45,23 @@ public final class SnowBallLauncher extends JavaPlugin implements Listener {
     @EventHandler
     public void onThrow (PlayerInteractEvent event) {
         ItemStack item = event.getItem();
-        Player player = event.getPlayer();
+        Player pl = event.getPlayer();
+        if (item == null) return;
         if (item.getType() == Material.SNOW_BALL) {
-            player.launchProjectile(Snowball.class);
             event.setCancelled(true);
+            if (event.getAction() != Action.RIGHT_CLICK_AIR &&
+                event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+            pl.launchProjectile(Snowball.class);
+            pl.getWorld().playSound(pl.getLocation(), Sound.BLOCK_GRASS_PLACE, 0.5F, 0.5F);
         }
+    }
+
+    @EventHandler
+    public void onHit (ProjectileHitEvent event) {
+        Entity prj = event.getEntity();
+        prj.getWorld().spawnParticle(Particle.BLOCK_CRACK, prj.getLocation(), 10, 0, 0, 0, 1.0F, new MaterialData(Material.ICE));
+        if (event.getHitBlock() != null)
+            prj.getWorld().playSound(prj.getLocation(), Sound.BLOCK_GLASS_BREAK, 0.5F, 5.0F);
     }
 
     /**
@@ -54,6 +71,6 @@ public final class SnowBallLauncher extends JavaPlugin implements Listener {
     @EventHandler
     public void onLogin (PlayerLoginEvent event) {
         Bukkit.getScheduler().runTaskLater(
-                this, () -> {event.getPlayer().getInventory().addItem(sball);}, 20 * 1);
+                this, () -> event.getPlayer().getInventory().setItem(1, sball), 20);
     }
 }
